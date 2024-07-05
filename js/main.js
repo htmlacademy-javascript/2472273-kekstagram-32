07d2de9ce6bda99c700bcbdae88acaafc748939b
VtElizaveta <pogotovkoliza@mail.ru>
2024-07-05 09:33:01 +0400

    Добавляет изменения после проверки

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 const NUMBER_OF_DESCRIPTIONS = 25;
+const MIN_ID_OBJECT = 1;
+const MAX_ID_OBJECT = 25;
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+const MIN_COMMENTS = 0;
+const MAX_COMMENTS = 30;
+const MIN_NUMBER_AVATAR = 1;
+const MAX_NUMBER_AVATAR = 6;
+const MIN_ID_COMMENT = 0;
+const MAX_ID_COMMENT = NUMBER_OF_DESCRIPTIONS * MAX_COMMENTS;
 
-const INDEX = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+const PHOTOS_NUMBER = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 
 const TEXT_COMMENT = [
   'Всё отлично!',
@@ -55,16 +65,6 @@ const getRandomInteger = (min, max) => {
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-// функция для поиска числа в массиве
-function contains(array, value) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === value) {
-      return true;
-    }
-  }
-  return false;
-}
-
 // функция для поиска случайного уникального идентификатора
 function randomUniqId(min, max) {
   const UNIQ_ARRAY = [];
@@ -75,7 +75,7 @@ function randomUniqId(min, max) {
       return null;
     }
 
-    while (contains(UNIQ_ARRAY, randomId) === true){
+    while (UNIQ_ARRAY.includes(randomId)){
       randomId = getRandomInteger(min, max);
     }
 
@@ -85,18 +85,15 @@ function randomUniqId(min, max) {
 }
 
 // Для создания случайного Id объекта
-const getRandomIdObject = randomUniqId(1, 25);
+const getRandomIdObject = randomUniqId(MIN_ID_OBJECT, MAX_ID_OBJECT);
 
 // Для создания случайного Id комментария
-const getRandomIdComment = randomUniqId(0, 200);
-
-// Для создания случайного количества комментариев
-const getRandomNumberOfComments = randomUniqId(1, 30);
+const getRandomIdComment = randomUniqId(MIN_ID_COMMENT, MAX_ID_COMMENT);
 
 // функция, генерирующая комментарий
 const getComment = () => ({
   id: getRandomIdComment(),
-  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+  avatar: `img/avatar-${getRandomInteger(MIN_NUMBER_AVATAR, MAX_NUMBER_AVATAR)}.svg`,
   message: getRandomArrayElement(TEXT_COMMENT),
   name: getRandomArrayElement(NAME),
 });
@@ -104,10 +101,10 @@ const getComment = () => ({
 // Функция, генерирующая объекты массива - описание фотографии
 const descriptionOfTheObject = () => ({
   id: getRandomIdObject(),
-  url: `photos/${getRandomArrayElement(INDEX)}.jpg`,
+  url: `photos/${getRandomArrayElement(PHOTOS_NUMBER)}.jpg`,
   description: getRandomArrayElement(PHOTO_DESCRIPTION),
-  likes: getRandomInteger(15, 200),
-  comments: Array.from({length: getRandomNumberOfComments()}, getComment),
+  likes: getRandomInteger(MIN_LIKES, MAX_LIKES),
+  comments: Array.from({length: getRandomInteger(MIN_COMMENTS, MAX_COMMENTS)}, getComment),
 });
 
 // функция, создающая массив из объектов, которые описывают фотографии
